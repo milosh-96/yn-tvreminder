391b988b58793e2ca082022b9b55799445ff3630
milosh-96 <milosjovanovic042@gmail.com>
2018-08-17 22:45:49 +0200

    added user home template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   <img src="{{Storage::url('public/logo.png')}}" alt="Logo">
+                   <img src="{{asset('images/logo.png')}}" alt="Logo">
                 </div>
 
                 <div class="links">
