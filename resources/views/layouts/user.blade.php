c05ae7d06fc9f1f33bbdec88572eda630f6433f9
Miloš <milos@Markos-MacBook.local>
2018-08-20 05:05:25 +0200

    switch to bootstrap; styling add show page

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-all.min.css">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     
     <style>
@@ -15,14 +14,14 @@
 <body>
     <div class="container-fluid mb-2 topbar">
         <div class="row">
-            <div class="cell-12 text-left">
+            <div class="col-12 text-left">
                 <img src="{{asset('images/logo.png')}}" height="30px">
             </div>
         </div>
     </div>
     <div class="container pb-5 page">
         <div class="row header">
-            <div class="cell-3">
+            <div class="col-3">
                 <div class="user">
                     <div class="d-inline-block">
                         <div class="circle">
@@ -32,15 +31,15 @@
                     <div class="d-inline-block">Miloš</div>
                 </div>
             </div>
-            <div class="cell-9 header-area">
+            <div class="col-9 header-area">
             </div>
         </div>
         <div class="row mt-4">
-            <div class="cell-12 order-2 order-md-1 cell-md-3">
+            <div class="col-12 col-md-3">
                 @include('user.partials.user-menu')
             </div>
             
-            <div class="cell-12 order-1 order-md-2 cell-md-7 offset-md-1 mb-5 mb-md-0">
+            <div class="col-12 col-md-7 offset-md-1 mb-5 mb-md-0">
                 <div class="row">
                    @yield('content')
                 </div>
@@ -49,7 +48,16 @@
         </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+    <script>
+        function activateOneTime() {
+            $("#date-picker").show();
+            $("#days-picker").hide();
+        }
+        function activateWeekly() {
+            $("#date-picker").hide();
+            $("#days-picker").show();
+        }
+    </script>
 </body>
 </html>
--- resources/views/user/add-show.blade.php
+++ resources/views/user/add-show.blade.php
@@ -1,32 +1,79 @@
 @extends('layouts.user')
 @section('content')
 
- <div class="cell-12">
-                        <form action="#">
-                            <div class="form-group">
-                                <label>Title</label>
-                                <input type="text" placeholder="Enter a title" />
-                                <small class="text-muted">Doesn't have to be a real show name. Type here how you remembered show.</small>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="cell-12 cell-md-6">
-                                        <label>Begins On</label>
-                                        <input type="text" data-role="datepicker"> 
-                                        <small class="text-muted">Date when the show starts to air. Doesn't have to be correct date, if you don't know.</small>
-                                    </div>
-                                    <div class="cell-12 cell-md-6">
-                                        <label>Show Start Time</label>
-                                        <input data-role="timepicker">
-                                        <small class="text-muted">Time when shows starts on TV.</small>
-                                    </div>
-                                    
+<div class="col-12">
+    <form action="">
+        <div class="form-group">
+            <label for="title"><i class="fa fa-font"></i>Title</label>
+            <span class="hint">You can type anything that will help you to remember this show</span>
+            <input id="title" type="text" class="form-control form-control-lg"> 
+        </div>
+        <div class="form-group">
+            <label for="description"><i class="fa fa-comment"></i> Description</label>
+            <span class="hint">How would you describe this show? Be descriptive, or ignore this field :)</span>
+            <textarea name="description" id="description" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <label><i class="fa fa-clock"></i> Scheduler</label>
+            <span class="hint">Set the date/days and time when this show is being aired.</span>
+            
+            <div class="row">
+                    <div class="col-12 mb-2">
+                            <div class="tyle-selector py-2">
+                                    <label>One-Time</label>
+                                    <input type="radio" name="repeat_type" onclick="activateOneTime()">
+                                    <span class="px-3">or</span>  
+                                    <label>Weekly</label>
+                                    <input type="radio" checked name="repeat_type" onclick="activateWeekly()">
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Description</label>
-                                <textarea data-role="textarea"></textarea>
-                            </div>
-                        </form>
+                       </div>
+                <div class="col-12 col-md-6">
+                    <div id="date-picker" class="date-picker collapse">
+                        <select class="form-control w-25 d-inline" name="day" id="day">
+                            @foreach($formValues->days as $day) 
+                            <option value="{{$day}}">{{$day}}</option>
+                            @endforeach
+                        </select>
+                        <select class="form-control w-25 d-inline" name="month" id="month">
+                            @foreach($formValues->months as $month) 
+                            <option value="{{$month}}">{{$month}}</option>
+                            @endforeach
+                        </select>
+                        <select class="form-control w-25 d-inline" name="year" id="year">
+                            @foreach($formValues->years as $year) 
+                            <option value="{{$year}}">{{$year}}</option>
+                            @endforeach
+                        </select>
                     </div>
-@endsection
+                    <div id="days-picker" class="days-picker">
+                        <select name="days[]" multiple id="days" class="form-control">
+                            <option value="1">Monday</option>
+                            <option value="2">Tueday</option>
+                            <option value="3">Wednesday</option>
+                            <option value="4">Thursday</option>
+                            <option value="5">Friday</option>
+                            <option value="6">Saturday</option>
+                            <option value="7">Sunday</option>
+                        </select>
+                    </div>
+                </div>
+                    <div class="col-12 col-md-6">
+                        <div class="time-picker">
+                            <select class="form-control w-25 d-inline" name="hour" id="hour">
+                                @foreach($formValues->hours as $hour) 
+                                <option value="{{$hour}}">{{$hour}}</option>
+                                @endforeach
+                            </select>
+                            <select class="form-control w-25 d-inline" name="minute" id="minute">
+                                @foreach($formValues->minutes as $minute) 
+                                <option value="{{$minute}}">{{$minute}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </form>
+    </div>
+    @endsection
