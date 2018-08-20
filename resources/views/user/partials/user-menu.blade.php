cb5e1d94379af0f73be6729f8e9c234d099894fe
Miloš <milos@Markos-MacBook.local>
2018-08-20 05:27:54 +0200

    added item popular shows item to the user menu

--- resources/views/user/partials/user-menu.blade.php
+++ resources/views/user/partials/user-menu.blade.php
@@ -5,6 +5,6 @@
     <li class="list-group-item"><a href="#"><span class="fa fa-plus icon icon"></span> Add New Show</a></li>
     <li class="list-group-item"><a href="#"><span class="fa fa-list icon icon"></span> List All</a></li>
     <li class="list-group-item"><a href="#"><span class="fa fa-question icon icon"></span> Help</a></li>
-    <li class="list-group-item section-title">Support</li>
-    <li class="list-group-item">
+    <li class="list-group-item section-title">Suggestions</li>
+    <li class="list-group-item"><i class="fa fa-star"></i> Most Popular Shows</li>
     </ul>
