628999f955a9cb53337fd96143db66c6b529d79c
milosh-96 <milosjovanovic042@gmail.com>
2018-10-10 04:10:30 +0200

    removed user hash from migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('hash',8);
+            //$table->string('hash',8);
             $table->string('user_name')->unique();
             $table->string('first_name');
             $table->string('last_name');
