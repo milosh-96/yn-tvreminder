c2b2a440942e47ddaea29d1fa6356ebd65f71701
milosh-96 <milosjovanovic042@gmail.com>
2018-10-19 04:05:19 +0200

    simple fix

--- public/sw.js
+++ public/sw.js
@@ -2,14 +2,15 @@ self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log("[Service Worker] Push had this data");
   
-    const title = 'Bejzbol: Astros - Red Sox';
-    const options = {
-      body: 'The event start in 15 minutes (16:03) on Arena Sport 2!',
-      icon: 'images/logo.png',
-      badge:'images/logo.png'
-    };
+    let data = event.data.json();
+      let title = data.title;
+      let options = {
+        body: data.body,
+        icon: 'images/logo.png',
+        badge:'images/logo.png'
+      };
+   
 
-    console.log(event);
     event.waitUntil(self.registration.showNotification(title,options));
   });
   
