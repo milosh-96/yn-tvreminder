befb05efb489676841f6d78b0f4e69659f0f8dc9
milosh-96 <milosjovanovic042@gmail.com>
2018-10-30 01:10:48 +0100

    Reminder Create: TV field fix

--- resources/views/show/partials/scheduler.blade.php
+++ resources/views/show/partials/scheduler.blade.php
@@ -78,7 +78,7 @@
                             @endforeach
                         </select>
                         <span class="d-inline px-2">on</span>
-                        <input type="text" class="w-25 form-control d-inline" name="tv_channel" placeholder="TV channel" value="{{$formValues->tv}}">
+                        <input type="text" class="w-25 form-control d-inline" name="tv_channel" placeholder="TV channel" value="@if($formValues->type == "create") $formValues->tv @endif">
                     </div>
                     <hr>
                     <div class="form-check">
