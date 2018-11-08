dd2696058c10a7656f96606755055d81dbf2f838
milosh-96 <milosjovanovic042@gmail.com>
2018-11-08 03:53:42 +0100

    Tips Slider: Add/Edit Show  page

--- resources/views/user/add-show.blade.php
+++ resources/views/user/add-show.blade.php
@@ -48,7 +48,16 @@
 @endsection
 
 @section('left_side')
-<div class="card" style="opacity: 0.5">
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="card" style="opacity: 0.5">
     <div class="card-body">
         <h5 class="card-title">Help</h5>
         <p class="card-text">
@@ -58,14 +67,29 @@
         </p>
     </div>
 </div>
-<hr>
-<div class="card" style="opacity: 0.5">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+    <div class="card" style="opacity: 0.5">
     <div class="card-body">
         <h5 class="card-title">Tips</h5>
         <p class="card-text">
             You can add anything! It can be series of the shows (two shows in a row on some TV channel), web show (on Netflix, Hulu or You Tube) or anything you like!   
         </p>
     </div>
+</div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
 
 @endsection
